0166a8d02d4f99b20f3a9cdb503a9bf72f0d9dd2
Aman <azadaman4321@gmail.com>
2026-06-06 10:19:09 +0530

    sum3

---
+++ recursion/combinationSum3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void Find(int k,int target,int i,List<List<Integer>>ans,List<Integer>temp,int sum)
+    {
+        if(sum==target && temp.size() == k)
+        {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        if(sum>target||i>9||temp.size()>k)
+            return;
+        temp.add(i);
+        Find(k,target,i+1,ans,temp,sum+i);
+        temp.remove(temp.size()-1);
+        Find(k,target,i+1,ans,temp,sum);
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>temp=new ArrayList<>();
+        Find(k,n,1,ans,temp,0);
+        return ans;
+    }
+}
